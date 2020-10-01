70ed254611336e14976ca09e57de7641c8cceaf0
Ankit Jee <ankitjee2000@gmail.com>
2020-10-01 22:26:01 +0530

    Minimised lines of code

--- lakshmi.cpp
+++ lakshmi.cpp
@@ -8,26 +8,9 @@ int main()
     int a, b,max;
     cout<<"enter two numbers: ";
     cin>>a>>b;
-    if(a>b)
-        max = a;
-    else
-        max = b;
-    cout<<max<<" is a largest number";
-
-    max = 9;
-
-     max = 5;
+    max=a>b?a:b;
     
-
-
-    cout<<"change 1";
-    //second change
-    //third change
-    //fourth change
-    //fifth change
-    //sixth change
-
-    //new branch
+    cout<<max<<" is a largest number";
 
     getch();
     return 0;
